bfb2ded7677fbdd8b5c317a329f65664abd0f710
ayush855-cloud <ayush.agr20@gmail.com>
2021-05-20 02:30:07 +0530

    first commit

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+// import './Header.css';
+
+function Header() {
+    return (
+        <>
+          <h1>Hangman</h1>  
+          <p>Find the Hidden word-Enter a letter</p>
+        </>
+    )
+}
+
+export default Header
